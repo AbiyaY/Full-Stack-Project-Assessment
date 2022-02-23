04e3b7f65248df6edf70fdaa81a0612d2b38e234
AbiyaY <abiyay30@gmail.com>
2022-02-23 01:15:15 +0000

    Simple Header and Footer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
+import React from "react";
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
+      <Header />
+      <Footer/>
     </div>
+    
   );
 }
 
---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h4>Video Recommendation</h4>
+        </header>
+    );
+};
+
+export default Header;
